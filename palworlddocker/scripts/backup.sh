1a67a52d9929e92dfb277f0ce9cf8e5cb8d14895
pavel00718 <pablo00718@gmail.com>
2024-01-24 18:32:45 +0100

    Delete palworlddocker directory

--- palworlddocker/scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-FILE_PATH="/palworld/backups/palworld-save-${DATE}.tar.gz"
-cd /palworld/Pal/ || exit
-
-tar -zcf "$FILE_PATH" "Saved/"
-echo "backup created at $FILE_PATH"
